27d92bfa8d999359b3843232d3c8801b380a9b43
unknown <michaelmolina679@gmail.com>
2022-11-07 16:37:11 -0600

    usando expres para crear una request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 const notes = [
     {
@@ -21,11 +22,38 @@ const notes = [
     }
 ]
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.end(JSON.stringify(notes));
-});
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, {'Content-Type': 'application/json'});
+//     response.end(JSON.stringify(notes));
+// });
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Worlds</h1>');
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes);
+})
+
+app.get('/api/notes/:id', (request, response) => {
+
+    const id = Number(request.params.id);
+    const note = notes.find(note => note.id === id);
+    
+    if(note) {
+        response.json(note);
+    } else {
+        response.status(404).end();
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id);
+    notes = notes.filter(note => note.id != id);
+    response.status(204).end();
+})
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server corriendo en puerto ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server corriendo en puerto ${PORT}`);
+});
